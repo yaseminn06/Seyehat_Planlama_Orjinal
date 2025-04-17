eea9673cb1ad5de0aeae056313bec16c9cf6d596
Yasemin <yasmings635@gmail.com>
2025-04-18 02:48:11 +0300

    giriş ekranına yönlendirme

--- Seyehat_Planlama_V/Form1.Designer.cs
+++ Seyehat_Planlama_V/Form1.Designer.cs
@@ -28,13 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(546, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(729, 532);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +58,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Seyehat_Planlama_V/Form2.cs
+++ Seyehat_Planlama_V/Form2.cs
@@ -10,18 +10,11 @@ using System.Windows.Forms;
 
 namespace Seyehat_Planlama_V
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-          Form2 form2 = new Form2();
-            form2.Show();
-            this.Hide();
         }
     }
 }
